d4264fcd0dc578e0442e36a7b6155a99b91bf3f3
Ehtesham <ehte6848@gmail.com>
2018-01-27 00:57:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ehtesham/mapshack/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.ehtesham.mapshack;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button b = findViewById(R.id.mapsBtnId);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent t = new Intent(MainActivity.this,MapsActivity.class);
+                startActivity(t);
+            }
+        });
+    }
+}
